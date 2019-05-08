f30ab606b387c8b238d8b1bd216aab9b7ab0c1c2
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2019-05-07 22:43:52 -0700

    WIP update to julia 0.7

--- src/SynapseClient.jl
+++ src/SynapseClient.jl
@@ -2,77 +2,88 @@ module SynapseClient
 
 using Compat
 using PyCall
-@pyimport synapseclient
-@pyimport synapseclient.client as pyclient
-@pyimport synapseclient.annotations as pyannotations
-@pyimport synapseclient.dict_object as pydict_object
-@pyimport synapseclient.cache as pycache
 
-import Base: get
+synapseInstalled = try
+	dummy = pyimport("synapseclient")
+	true
+catch e
+	false
+end
 
-export
-	Synapse,
 
-	# SynapseClient
-	chunkedquery,
-	delete,
-	deleteprovenance,
-	downloadtablecolumns,
-	downloadtablefile,
-	#get, # NB: it makes more sense to extend Base.get since it is a dictionary lookup
-	getannotations,
-	getcolumn,
-	getcolumns,
-	getconfigfile,
-	getevaluation,
-	getevaluationbycontentsource,
-	getevaluationbyname,
-	getpermissions,
-	getprovenance,
-	getsubmission,
-	getsubmissionbundles,
-	getsubmissionstatus,
-	getsubmissions,
-	gettablecolumns,
-	getteam,
-	getteammembers,
-	getuserprofile,
-	getwiki,
-	getwikiheaders,
-	invalidateapikey,
-	login,
-	logout,
-	md5query,
-	onweb,
-	printentity,
-	restdelete,
-	restget,
-	restpost,
-	restput,
-	sendmessage,
-	setannotations,
-	setendpoints,
-	setpermissions,
-	setprovenance,
-	store,
-	submit,
-	tablequery,
-	updateactivity,
-	uploadfile,
+if synapseInstalled
+	@pyimport synapseclient
+	@pyimport synapseclient.client as pyclient
+	@pyimport synapseclient.annotations as pyannotations
+	@pyimport synapseclient.dict_object as pydict_object
+	@pyimport synapseclient.cache as pycache
 
-	# Entity
-	create,
+	import Base: get
 
-	# Activity
-	used,
-	executed
+	export
+		Synapse,
 
-include("wrapper.jl")
-include("types.jl")
-include("synapse.jl")
-include("utils.jl")
-include("entity.jl")
-include("annotations.jl")
-include("cache.jl")
+		# SynapseClient
+		chunkedquery,
+		delete,
+		deleteprovenance,
+		downloadtablecolumns,
+		downloadtablefile,
+		#get, # NB: it makes more sense to extend Base.get since it is a dictionary lookup
+		getannotations,
+		getcolumn,
+		getcolumns,
+		getconfigfile,
+		getevaluation,
+		getevaluationbycontentsource,
+		getevaluationbyname,
+		getpermissions,
+		getprovenance,
+		getsubmission,
+		getsubmissionbundles,
+		getsubmissionstatus,
+		getsubmissions,
+		gettablecolumns,
+		getteam,
+		getteammembers,
+		getuserprofile,
+		getwiki,
+		getwikiheaders,
+		invalidateapikey,
+		login,
+		logout,
+		md5query,
+		onweb,
+		printentity,
+		restdelete,
+		restget,
+		restpost,
+		restput,
+		sendmessage,
+		setannotations,
+		setendpoints,
+		setpermissions,
+		setprovenance,
+		store,
+		submit,
+		tablequery,
+		updateactivity,
+		uploadfile,
+
+		# Entity
+		create,
+
+		# Activity
+		used,
+		executed
+
+	include("wrapper.jl")
+	include("types.jl")
+	include("synapse.jl")
+	include("utils.jl")
+	include("entity.jl")
+	include("annotations.jl")
+	include("cache.jl")
+end
 
 end
